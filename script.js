d3f6bc583dad5dacc7ffb0537b60b33c5f107844
nabilAhmedN <nabilahmednahid@gmail.com>
2022-09-03 18:53:26 +0600

    show the news contant and loading spinner added

--- script.js
+++ script.js
@@ -32,7 +32,78 @@ const loadNews = async(category_id,category_name) =>{
     const res = await fetch(url);
     const data = await res.json();
     displayNews(data.data);
-    const showNews = document.getElementById('input-feild');
+    const showNews = document.getElementById('show-contant-feild');
     const showNewsValue = `${data.data.length} items found for this category ${category_name}` ;
-    showNews.value = showNewsValue;
+    showNews.innerText = showNewsValue;
 }
+const displayNews = (data) =>{
+    
+    // sort by view
+    data.sort((a, b) => b.total_view- a.total_view);
+    const newsContainer = document.getElementById('itme-news');
+    newsContainer.textContent= '';
+
+    data.forEach(news =>{
+        console.log(news);
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('card');
+        newsDiv.classList.add('mb-3');
+        
+        console.log(news);
+        newsDiv.innerHTML=`
+        <div class="row">
+        <div class="col-md-4">
+            <img src="${news.image_url}" alt="" class="img-fluid h-100 ">
+        </div>
+        <div class="col-md-8 p-5 ">
+            <h3>${news.title}</h3>
+            <p>${news.details.length > 506 ? news.details.slice(0,506) +'...' : news.details }</p>
+
+                <div class="d-flex align-items-center justify-content-between">
+                    <div class="d-flex align-items-center ">
+                        <img src="${news.author.img}" alt="" height="30" class="rounded-circle">
+                        <div >
+                            <p>${news.author.name} </p>
+                            <p>${news.author.published_date}</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <span><i class="fa-sharp fa-solid fa-eye"></i> ${news.total_view}M</span>
+
+                    </div>
+
+                    <div>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+
+                    <div>
+                        <i class="fa-solid fa-arrow-right" onclick ="loadNewsDetails('${news._id}')" data-bs-toggle="modal" data-bs-target="#newsDetailModal"></i>
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+
+    });
+    
+    toggleSpinner(false);
+}
+
+const toggleSpinner = isSpinner =>{
+    const spinnerSection = document.getElementById('spinner');
+    if(isSpinner){
+        spinnerSection.classList.remove('d-none');
+    }
+    else{
+        spinnerSection.classList.add('d-none');
+    }
+}
+
+loadNews('1');
